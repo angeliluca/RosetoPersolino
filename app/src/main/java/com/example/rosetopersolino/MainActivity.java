ce67cd87e3066b208c01a3073d0c83e1b4aa4beb
Luca Angeli <lucaproange@gmail.com>
2019-11-20 20:48:30 +0100

    Primo file - Applicazione vuota

--- app/src/main/java/com/example/rosetopersolino/MainActivity.java
+++ app/src/main/java/com/example/rosetopersolino/MainActivity.java
@@ -3,6 +3,9 @@
 *   Se leggete questi commenti siete dei boss e la condivisione dei file funziona
 *  help desk - Luca Angeli - contattare orario pasti
 *
+*
+*  Ciao Ila, riesci a leggere? Lo Zecchino d'oro è brutto.
+*  Commento in xml <!-- -->
 * */
 
 package com.example.rosetopersolino;
