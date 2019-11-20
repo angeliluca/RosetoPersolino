06abe570ccf58ff07d35289f77794b2142cabd1d
Luca Angeli <lucaproange@gmail.com>
2019-11-20 20:28:28 +0100

    Primo file - Applicazione vuota

---
+++ app/src/main/java/com/example/rosetopersolino/MainActivity.java
@@ -0,0 +1,21 @@
+//App per roseto Persolino - ILA - GIULIA - ALE - Luca
+/*
+*   Se leggete questi commenti siete dei boss e la condivisione dei file funziona
+*  help desk - Luca Angeli - contattare orario pasti
+*
+* */
+
+package com.example.rosetopersolino;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
