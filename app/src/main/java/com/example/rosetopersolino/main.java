0422929f38fadecf3f9292e2e3da61c1e8f80652
Giulia <giuliagamberini.g@gmail.com>
2019-11-22 21:46:57 +0100

    Prova di caricamento file già fatti

--- app/src/main/java/com/example/rosetopersolino/main.java
+++ app/src/main/java/com/example/rosetopersolino/main.java
@@ -2,7 +2,9 @@ package com.example.rosetopersolino;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class main extends AppCompatActivity {
 
@@ -12,3 +14,7 @@ public class main extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
     }
 }
+    public void visita(View view) {
+        Intent intent = new Intent(this, Visita.class);
+        startActivity(intent);
+    }
